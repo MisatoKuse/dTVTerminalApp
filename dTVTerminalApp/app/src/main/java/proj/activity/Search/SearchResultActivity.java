4e930bf0751d7a872fa09949decf9dde23a90a5c
liupan <ryuhan@fsi.co.jp>
2017-09-26 15:07:35 +0900

    検索結果画面の並び替え/絞込メニューボタンの追加

--- dTVTerminalApp/app/src/main/java/proj/activity/Search/SearchResultActivity.java
+++ dTVTerminalApp/app/src/main/java/proj/activity/Search/SearchResultActivity.java
@@ -2,8 +2,10 @@ package proj.activity.Search;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by hitue-fsi on 2017/09/22.
@@ -17,6 +19,7 @@ public class SearchResultActivity extends BaseActivity implements View.OnClickLi
     private Button bt_toDetail1;
     private Button bt_toDetail2;
     private Button bt_backTop2;
+    private FloatingActionButton fab_sort;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +27,12 @@ public class SearchResultActivity extends BaseActivity implements View.OnClickLi
         setContentView(R.layout.serch_result_main_layout);
         bt_toDetail1 = (Button) findViewById(R.id.bt_result1);
         bt_toDetail2 = (Button) findViewById(R.id.bt_result2);
-        bt_backTop2= (Button) findViewById(R.id.bt_backTop2);
+        bt_backTop2 = (Button) findViewById(R.id.bt_backTop2);
+        fab_sort = (FloatingActionButton) findViewById(R.id.fab);
         bt_toDetail1.setOnClickListener(this);
         bt_toDetail2.setOnClickListener(this);
         bt_backTop2.setOnClickListener(this);
+        fab_sort.setOnClickListener(this);
     }
 
     @Override
@@ -38,6 +43,9 @@ public class SearchResultActivity extends BaseActivity implements View.OnClickLi
                 break;
             case R.id.bt_result2:
                 startActivity(ChannelDetailPlayerActivity.class, null);
+                break;
+            case R.id.fab:
+                Toast.makeText(this, "人気順で並び替えました", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.bt_backTop2:
                 finish();
