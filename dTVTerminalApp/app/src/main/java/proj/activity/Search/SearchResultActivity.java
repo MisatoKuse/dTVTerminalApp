f51a65b45133d4884c493b887a9f6f04b260176b
liupan <ryuhan@fsi.co.jp>
2017-09-26 19:15:48 +0900

    検索履歴ともっと見るを削除しました。

--- dTVTerminalApp/app/src/main/java/proj/activity/Search/SearchResultActivity.java
+++ dTVTerminalApp/app/src/main/java/proj/activity/Search/SearchResultActivity.java
@@ -16,8 +16,8 @@ import proj.dtvterminalapp.R;
 public class SearchResultActivity extends BaseActivity implements View.OnClickListener {
     private Button bt_backTop2;
     private FloatingActionButton fab_sort;
-    private RelativeLayout mrelativeLayout;
     private RelativeLayout mrelativeLayout1;
+    private RelativeLayout mrelativeLayout2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,22 +25,22 @@ public class SearchResultActivity extends BaseActivity implements View.OnClickLi
         setContentView(R.layout.serch_result_main_layout);
         bt_backTop2 = findViewById(R.id.bt_backTop2);
         fab_sort = findViewById(R.id.fab);
-        mrelativeLayout = findViewById(R.id.Search_result);
         mrelativeLayout1 = findViewById(R.id.Search_result1);
+        mrelativeLayout2=findViewById(R.id.Search_result2);
         bt_backTop2.setOnClickListener(this);
         fab_sort.setOnClickListener(this);
-        mrelativeLayout.setOnClickListener(this);
         mrelativeLayout1.setOnClickListener(this);
+        mrelativeLayout2.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
 
-            case R.id.Search_result:
+                        case R.id.Search_result1:
                 startActivity(ChannelDetailPlayerActivity.class, null);
                 break;
-            case R.id.Search_result1:
+            case R.id.Search_result2:
                 startActivity(ChannelDetailPlayerActivity.class, null);
                 break;
             case R.id.fab:
--- dTVTerminalApp/app/src/main/java/proj/activity/Search/SearchTopActivity.java
+++ dTVTerminalApp/app/src/main/java/proj/activity/Search/SearchTopActivity.java
@@ -4,37 +4,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 
-
+import proj.activity.Player.ChannelDetailPlayerActivity;
 import proj.common.BaseActivity;
 import proj.dtvterminalapp.R;
 
 public class SearchTopActivity extends BaseActivity implements View.OnClickListener {
-    private Button bt_toResult;
-    private Button bt_toHistory;
+
     private Button bt_backHome;
+    private RelativeLayout mrelativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.serch_top_main_layout);
-        bt_toResult = (Button) findViewById(R.id.bt_keyword);
-        bt_toHistory = (Button) findViewById(R.id.bt_seeMore);
         bt_backHome = (Button) findViewById(R.id.bt_backHome);
-        bt_toResult.setOnClickListener(this);
-        bt_toHistory.setOnClickListener(this);
+        mrelativeLayout = findViewById(R.id.Search_result);
         bt_backHome.setOnClickListener(this);
+        mrelativeLayout.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.bt_keyword:
-                startActivity(SearchResultActivity.class,null);
-                break;
-            case R.id.bt_seeMore:
-                startActivity(SearchHistoryListActivity.class,null);
+            case R.id.Search_result:
+                startActivity(ChannelDetailPlayerActivity.class, null);
                 break;
             case R.id.bt_backHome:
                 finish();
