5adf1ac70b9c8793910d44dd349a8f9abbe68b66
liupan <ryuhan@fsi.co.jp>
2017-09-27 09:40:27 +0900

    下記のファイルの削除 Search/SearchHistoryListActivity.java Search/SearchResultActivity.java search/layout/search_history_list_main_layout.xml search/layout/search_history_main_layout.xml search/layout/serch_result_main_layout.xml

--- dTVTerminalApp/app/src/main/java/proj/activity/Search/SearchTopActivity.java
+++ dTVTerminalApp/app/src/main/java/proj/activity/Search/SearchTopActivity.java
@@ -17,6 +17,8 @@ public class SearchTopActivity extends BaseActivity implements View.OnClickListe
     private FloatingActionButton fab_sort;
     private Button bt_backHome;
     private RelativeLayout mrelativeLayout;
+    private RelativeLayout mrelativeLayout1;
+    private RelativeLayout mrelativeLayout2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +27,26 @@ public class SearchTopActivity extends BaseActivity implements View.OnClickListe
         bt_backHome = findViewById(R.id.bt_backHome);
         mrelativeLayout = findViewById(R.id.Search_result);
         fab_sort = findViewById(R.id.fab);
+        mrelativeLayout1 = findViewById(R.id.Search_result1);
+        mrelativeLayout2 = findViewById(R.id.Search_result2);
         bt_backHome.setOnClickListener(this);
         mrelativeLayout.setOnClickListener(this);
         fab_sort.setOnClickListener(this);
+        mrelativeLayout1.setOnClickListener(this);
+        mrelativeLayout2.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+
             case R.id.Search_result:
+                startActivity(ChannelDetailPlayerActivity.class, null);
+                break;
+            case R.id.Search_result1:
+                startActivity(ChannelDetailPlayerActivity.class, null);
+                break;
+            case R.id.Search_result2:
                 startActivity(ChannelDetailPlayerActivity.class, null);
                 break;
             case R.id.fab:
