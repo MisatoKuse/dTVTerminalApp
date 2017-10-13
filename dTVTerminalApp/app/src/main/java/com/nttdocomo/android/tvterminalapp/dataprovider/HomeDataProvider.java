63e21325dbb922c160e9060ac72776019efc4d90
yanase <ymasatos@fsi.co.jp>
2017-10-13 14:10:53 +0900

    データプロバイダクラス定義

---
+++ dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/dataprovider/HomeDataProvider.java
@@ -0,0 +1,30 @@
+package com.nttdocomo.android.tvterminalapp.dataprovider;
+
+/**
+ * Copyright © 2018 NTT DOCOMO, INC. All Rights Reserved.
+ */
+
+public class HomeDataProvider {
+
+    public void getHomeData() {
+        //Activityからのデータ取得要求受付
+    }
+
+    public boolean checkHomeData() {
+        //Home用構造体を確認する
+        return false;
+    }
+
+    public boolean checkLastDate(){
+        //前回DB更新日時から一定時間が経過していたらfalseを返却
+        return false;
+    }
+
+    public void sendHomeData(){
+        //HomeActivityにHomeBeenを返却する
+    }
+
+    public void setStructData(){
+        //引数に渡された各オブジェクトに応じたDMに構造体を渡す(ポリモーフィズム)
+    }
+}
