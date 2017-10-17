43ca81934ff6a7344893f0e317e13537ef3fac6c
Mamoru, Banzawa <mbanzawa@fsi.co.jp>
2017-10-17 23:10:14 +0900

    フォルダ名変更（大文字→小文字）※一時変更

---
+++ dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/activity/tvprogram_temp/MyChannelEditActivity.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2018 NTT DOCOMO, INC. All Rights Reserved.
+ */
+
+package com.nttdocomo.android.tvterminalapp.activity.tvprogram_temp;
+
+/**
+ * Created by ryuhan on 2017/09/22.
+ */
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.nttdocomo.android.tvterminalapp.activity.BaseActivity;
+import com.nttdocomo.android.tvterminalapp.R;
+
+public class MyChannelEditActivity extends BaseActivity implements View.OnClickListener {
+
+    private Button btnBack,btnAdd;
+    private TextView channel1,channel2,channel3,channel4,channel5,channel6,backIcon;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.my_channel_edit_main_layout);
+        initView();
+    }
+
+    private void initView() {
+        btnBack = (Button)findViewById(R.id.btn_back);
+        btnAdd = (Button)findViewById(R.id.btn_add);
+        channel1= (TextView)findViewById(R.id.channel1);
+        channel2= (TextView)findViewById(R.id.channel2);
+        channel3= (TextView)findViewById(R.id.channel3);
+        channel4= (TextView)findViewById(R.id.channel4);
+        channel5= (TextView)findViewById(R.id.channel5);
+        channel6= (TextView)findViewById(R.id.channel6);
+        backIcon = (TextView)findViewById(R.id.back_key);
+        backIcon.setOnClickListener(this);
+        btnBack.setOnClickListener(this);
+        btnAdd.setOnClickListener(this);
+        channel1.setOnClickListener(this);
+        channel2.setOnClickListener(this);
+        channel3.setOnClickListener(this);
+        channel4.setOnClickListener(this);
+        channel5.setOnClickListener(this);
+        channel6.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_back:
+            case R.id.back_key:
+                finish();
+                break;
+            case R.id.btn_add:
+            case R.id.channel1:
+            case R.id.channel2:
+            case R.id.channel3:
+            case R.id.channel4:
+            case R.id.channel5:
+            case R.id.channel6:
+            case R.id.add_over:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                final AlertDialog dialog = builder.create();
+                Window window = dialog.getWindow();
+                window.setGravity(Gravity.BOTTOM);
+                View view1 = View.inflate(this, R.layout.channel_add_dialog_layout, null);
+                dialog.setView(view1,0,0,0,0);
+                dialog.show();
+                view1.findViewById(R.id.add_over).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+
+                break;
+        }
+    }
+}
---
+++ dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/activity/video_temp/VideoPurchListActivity.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2018 NTT DOCOMO, INC. All Rights Reserved.
+ */
+
+package com.nttdocomo.android.tvterminalapp.activity.video_temp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.nttdocomo.android.tvterminalapp.activity.player_temp.TvPlayerActivity;
+import com.nttdocomo.android.tvterminalapp.activity.BaseActivity;
+import com.nttdocomo.android.tvterminalapp.R;
+
+/**
+ * Created by hitue-fsi on 2017/09/22.
+ */
+
+public class VideoPurchListActivity extends BaseActivity {
+
+    private RelativeLayout mRelativeLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.video_purch_list_main_layout);
+        initView();
+    }
+
+    private void initView(){
+        mRelativeLayout = findViewById(R.id.purch_video_list_main_layout_ll1);
+        mRelativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(TvPlayerActivity.class,null);
+            }
+        });
+    }
+}
