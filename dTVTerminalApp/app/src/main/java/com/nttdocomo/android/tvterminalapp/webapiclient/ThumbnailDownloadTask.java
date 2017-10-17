07af4c1e203db6a6a969c7c283db235ad644c9a3
Mamoru, Banzawa <mbanzawa@fsi.co.jp>
2017-10-17 16:53:37 +0900

    フォルダ名変更

--- dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/webapiclient/ThumbnailDownloadTask.java
+++ dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/webapiclient/ThumbnailDownloadTask.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2018 NTT DOCOMO, INC. All Rights Reserved.
  */
 
-package com.nttdocomo.android.tvterminalapp.webapiclient_temp;
+package com.nttdocomo.android.tvterminalapp.webapiclient;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
