2e3409dc2885e0816463d657f44381c10d431d94
sasakiy <syoshiak@fsi.co.jp>
2017-10-30 14:07:05 +0900

    DREM-409 ([DREM-6]レンタル一覧) 実装（通信・JSONParser）
    購入済みVOD一覧取得：正常時レスポンスデータのデータクラス
    ・新規作成
    
    Change-Id: Icd2db0e08caf6b333a7a401fb035215ba64723fb

---
+++ dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/dataprovider/data/VodMetaFullResponse.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2018 NTT DOCOMO, INC. All Rights Reserved.
+ */
+
+package com.nttdocomo.android.tvterminalapp.dataprovider.data;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * 購入済みVOD一覧取得：正常時レスポンスデータ
+ */
+
+public class VodMetaFullResponse implements Serializable {
+
+    private static final long serialVersionUID = 11185212718261137L;
+
+    private String mStatus;                                 // status
+    private ArrayList<VodMetaFullData> mVodMetaFullData;  // 購入済みVOD一覧リスト
+
+    public static final String VOD_META_FULL_RESPONSE_STATUS = "status";
+
+    public String getStatus() {
+        return mStatus;
+    }
+
+    public void setStatus(String status) {
+        mStatus = status;
+    }
+
+    public ArrayList<VodMetaFullData> getVodMetaFullData() {
+        return mVodMetaFullData;
+    }
+
+    public void setVodMetaFullData(ArrayList<VodMetaFullData> vodMetaFullData) {
+        mVodMetaFullData = vodMetaFullData;
+    }
+
+    public VodMetaFullResponse() {
+        mStatus = "OK";     // OK 固定値
+        mVodMetaFullData = new ArrayList<VodMetaFullData>();  // 購入済みVOD一覧
+    }
+}
