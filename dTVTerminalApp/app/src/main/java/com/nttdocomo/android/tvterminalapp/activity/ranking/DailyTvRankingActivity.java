d508e2fd76f751167cb374544b79cdca95f67514
erinahin <erinahin@fsi.co.jp>
2017-10-31 18:01:05 +0900

    DREM-364 ([DREM-47-1]今日のテレビランキング) 実装（UI）
        ・List表示
        ・リロード処理
        ・データの取得
        ・FindBugs対応等
    
    Change-Id: I47cf274171ed7ef330a6049597f0e1aa054476bb

--- dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/activity/ranking/DailyTvRankingActivity.java
+++ dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/activity/ranking/DailyTvRankingActivity.java
@@ -5,35 +5,266 @@
 package com.nttdocomo.android.tvterminalapp.activity.ranking;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
 
-import com.nttdocomo.android.tvterminalapp.activity.player.TvPlayerActivity;
-import com.nttdocomo.android.tvterminalapp.activity.BaseActivity;
 import com.nttdocomo.android.tvterminalapp.R;
+import com.nttdocomo.android.tvterminalapp.activity.BaseActivity;
+import com.nttdocomo.android.tvterminalapp.activity.player.TvPlayerActivity;
+import com.nttdocomo.android.tvterminalapp.adapter.ContentsAdapter;
+import com.nttdocomo.android.tvterminalapp.common.ContentsData;
+import com.nttdocomo.android.tvterminalapp.common.DTVTLogger;
+import com.nttdocomo.android.tvterminalapp.dataprovider.RankingTopDataProvider;
+import com.nttdocomo.android.tvterminalapp.webapiclient.jsonparser.VideoRankJsonParser;
 
-public class DailyTvRankingActivity extends BaseActivity {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+public class DailyTvRankingActivity extends BaseActivity implements View.OnClickListener,
+        RankingTopDataProvider.ApiDataProviderCallback,
+        AbsListView.OnScrollListener, AdapterView.OnItemClickListener {
+
+    // 最大表示件数
+    private final static int NUM_PER_PAGE = 10;
+    // タイムアウト時間
+    private final static int LOAD_PAGE_DELAY_TIME = 1000;
+
+    private ImageView mMenuImageView;
+    private RankingTopDataProvider mRankingTopDataProvider;
+    private ContentsAdapter mContentsAdapter;
+
+    private View mLoadMoreView;
+    private ListView mListView;
+    private List mContentsList;
+
+    private boolean mIsCommunicating = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.daily_tv_ranking_main_layout);
+        mContentsList = new ArrayList();
+        mMenuImageView = findViewById(R.id.header_layout_menu);
+        mMenuImageView.setVisibility(View.VISIBLE);
+        mMenuImageView.setOnClickListener(this);
+        setTitleText(getString(R.string.daily_tv_ranking_title));
+        resetPaging();
+
+        initView();
+        mRankingTopDataProvider = new RankingTopDataProvider(this);
+        mRankingTopDataProvider.getRankingTopData();
     }
 
     /**
-     * コンテンツ詳細への遷移
-     *
-     * @param view
+     * ListViewの表示
      */
-    public void contentsDetailButton(View view) {
+    private void initView() {
+        if (mContentsList == null) {
+            mContentsList = new ArrayList();
+        }
+        mListView = findViewById(R.id.tv_rank_list);
+        mListView.setOnItemClickListener(this);
+        mListView.setOnScrollListener(this);
+        mContentsAdapter = new ContentsAdapter(
+                this,
+                mContentsList,
+                ContentsAdapter.ActivityTypeItem.TYPE_DAILY_RANK
+        );
+        mListView.setAdapter(mContentsAdapter);
+        mLoadMoreView = LayoutInflater.from(this).inflate(R.layout.search_load_more, null);
+    }
+
+    /**
+     * 再読み込み時の処理
+     */
+    private void resetCommunication() {
+        displayMoreData(false);
+        setCommunicatingStatus(false);
+    }
+
+    /**
+     * 読み込み表示を行う
+     *
+     * @param bool 読み込み表示フラグ
+     */
+    private void displayMoreData(boolean bool) {
+        if (null != mListView) {
+            if (bool) {
+                mListView.addFooterView(mLoadMoreView);
+            } else {
+                mListView.removeFooterView(mLoadMoreView);
+            }
+        }
+    }
+
+    /**
+     * ページングリセット
+     */
+    private void resetPaging() {
+        synchronized (this) {
+            setCommunicatingStatus(false);
+            if (0 != getCurrentNumber() && null != mContentsList) {
+                mContentsList.clear();
+                if (null != mContentsAdapter) {
+                    mContentsAdapter.notifyDataSetChanged();
+                }
+            }
+        }
+
+    }
+
+    /**
+     * 再読み込み実施フラグ設定
+     *
+     * @param bool 読み込み表示フラグ
+     */
+    private void setCommunicatingStatus(boolean bool) {
+        synchronized (this) {
+            mIsCommunicating = bool;
+        }
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view == mMenuImageView) {
+            onSampleGlobalMenuButton_PairLoginOk();
+        }
+    }
+
+    /**
+     * ページングを行った回数を取得
+     *
+     * @return ページング回数
+     */
+    private int getCurrentNumber() {
+        if (null == mContentsList || 0 == mContentsList.size()) {
+            return 0;
+        } else if (mContentsList.size() < NUM_PER_PAGE) {
+            return 1;
+        }
+        return mContentsList.size() / NUM_PER_PAGE;
+    }
+
+    @Override
+    public void onScroll(AbsListView absListView, int firstVisibleItem,
+                         int visibleItemCount, int totalItemCount) {
+        synchronized (this) {
+            if (null == mContentsAdapter) {
+                return;
+            }
+
+            if (firstVisibleItem + visibleItemCount == totalItemCount && 0 != totalItemCount) {
+                DTVTLogger.debug(
+                        "Activity::onScroll, paging, firstVisibleItem=" + firstVisibleItem
+                                + ", totalItemCount=" + totalItemCount
+                                + ", visibleItemCount=" + visibleItemCount);
+            }
+        }
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView absListView, int scrollState) {
+        synchronized (this) {
+            if (null == mContentsAdapter) {
+                return;
+            }
+            if (scrollState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE &&
+                    absListView.getLastVisiblePosition() == mContentsAdapter.getCount() - 1) {
+                if (mIsCommunicating) {
+                    return;
+                }
+                DTVTLogger.debug("onScrollStateChanged, do paging");
+                displayMoreData(true);
+                setCommunicatingStatus(true);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mRankingTopDataProvider.getRankingTopData();
+                    }
+                }, LOAD_PAGE_DELAY_TIME);
+            }
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        if (mLoadMoreView == view) {
+            return;
+        }
         startActivity(TvPlayerActivity.class, null);
     }
 
+    @Override
+    public void dailyRankListCallback(List<Map<String, String>> dailyRankMapList) {
+        setShowDailyRanking(dailyRankMapList);
+    }
+
     /**
-     * クリップボタン
-     *
-     * @param view
+     * 取得結果の設定・表示
      */
-    public void clipButton(View view) {
-        Toast.makeText(this, "クリップしました", Toast.LENGTH_SHORT).show();
+    private void setShowDailyRanking(List<Map<String, String>> dailyRankMapList) {
+        if (null == dailyRankMapList || 0 == dailyRankMapList.size()) {
+            return;
+        }
+        List<ContentsData> rankingContentInfo = setDailyRankContentData(dailyRankMapList);
+
+        int pageNumber = getCurrentNumber();
+        for (int i = pageNumber * NUM_PER_PAGE; i < (pageNumber + 1)
+                * NUM_PER_PAGE && i < rankingContentInfo.size(); ++i) {
+            DTVTLogger.debug("i = " + i);
+            if (null != mContentsList) {
+                mContentsList.add(rankingContentInfo.get(i));
+            }
+        }
+        resetCommunication();
+        mContentsAdapter.notifyDataSetChanged();
+    }
+
+    /**
+     * 取得したリストマップをContentsDataクラスへ入れる
+     *
+     * @param dailyRankMapList コンテンツリストデータ
+     * @return dataList 読み込み表示フラグ
+     */
+    private List<ContentsData> setDailyRankContentData(
+            List<Map<String, String>> dailyRankMapList) {
+        List<ContentsData> rankingContentsDataList = new ArrayList<>();
+
+        ContentsData rankingContentInfo;
+
+        for (int i = 0; i < dailyRankMapList.size(); i++) {
+            rankingContentInfo = new ContentsData();
+            rankingContentInfo.setRank(String.valueOf(i + 1));
+            rankingContentInfo.setThumURL(dailyRankMapList.get(i)
+                    .get(VideoRankJsonParser.VIDEORANK_LIST_THUMB));
+            rankingContentInfo.setTitle(dailyRankMapList.get(i)
+                    .get(VideoRankJsonParser.VIDEORANK_LIST_TITLE));
+            rankingContentInfo.setRatStar(dailyRankMapList.get(i)
+                    .get(VideoRankJsonParser.VIDEORANK_LIST_RATING));
+
+            rankingContentsDataList.add(rankingContentInfo);
+            DTVTLogger.info("RankingContentInfo " + rankingContentInfo.getRank());
+        }
+
+        return rankingContentsDataList;
+    }
+
+    @Override
+    public void weeklyRankCallback(List<Map<String, String>> weeklyHashMap) {
+        // NOP
+    }
+
+    @Override
+    public void videoRankCallback(List<Map<String, String>> videoHashMap) {
+        // NOP
     }
 }
---
+++ dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/adapter/ContentsAdapter.java
@@ -0,0 +1,214 @@
+/*
+ * Copyright (c) 2018 NTT DOCOMO, INC. All Rights Reserved.
+ */
+
+package com.nttdocomo.android.tvterminalapp.adapter;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.text.TextUtils;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.nttdocomo.android.tvterminalapp.R;
+import com.nttdocomo.android.tvterminalapp.common.ContentsData;
+import com.nttdocomo.android.tvterminalapp.dataprovider.ThumbnailProvider;
+
+import java.util.List;
+
+public class ContentsAdapter extends BaseAdapter {
+
+    //各Activityインスタンス
+    private Context mContext = null;
+    //リスト用データソース
+    private List<ContentsData> listData = null;
+    //サムネイル取得用プロバイダー
+    private ThumbnailProvider mThumbnailProvider = null;
+    //表示項目のタイプ
+    private ActivityTypeItem type;
+    //ビューの生成
+    private LayoutInflater mInflater;
+    //評価基準値
+    private final static int NUM_STARS_TOTAL = 5;
+    //サムネイル幅さ display3分の1
+    private final static int THUMBNAIL_WIDTH = 3;
+    //サムネイル高さ サムネイル幅さ2分の1
+    private final static int THUMBNAIL_HEIGHT = 2;
+    //サムネイルmarginleft
+    private final static int THUMBNAIL_MARGINLEFT = 15;
+    //サムネイルmargintop
+    private final static int THUMBNAIL_MARGINTOP = 10;
+    //サムネイルmarginright
+    private final static int THUMBNAIL_MARGINRIGHT = 8;
+    //サムネイルmarginbottom
+    private final static int THUMBNAIL_MARGINBOTTOM = 10;
+    //margin0
+    private final static int THUMBNAIL_MARGIN0 = 0;
+    //ライン高さ
+    private final static int LINE_HEIGHT = 1;
+
+    /**
+     * 機能
+     * 共通アダプター使う
+     */
+    public enum ActivityTypeItem {
+        TYPE_DAILY_RANK,          //今日のテレビランキング
+        TYPE_WEEKLY_RANK,         //週間テレビランキング
+        TYPE_VIDEO_RANK,          //ビデオランキング
+        TYPE_RENTAL_RANK          //レンタル
+    }
+
+    /**
+     * コンストラクタ
+     *
+     * @param mContext Activity
+     * @param listData リストデータ
+     * @param type     　項目コントロール
+     */
+    public ContentsAdapter(Context mContext, List<ContentsData> listData, ActivityTypeItem type) {
+        this.mContext = mContext;
+        mInflater = LayoutInflater.from(mContext);
+        this.listData = listData;
+        this.type = type;
+        mThumbnailProvider = new ThumbnailProvider(mContext);
+    }
+
+    @Override
+    public int getCount() {
+        return listData.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return listData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        ViewHolder holder;
+        //ビューの再利用
+        if (view == null) {
+            view = mInflater.inflate(R.layout.item_common_result, parent, false);
+            holder = new ViewHolder();
+            holder.rl_thumbnail = view.findViewById(R.id.item_common_result_thumbnail_rl);
+            holder.iv_thumbnail = view.findViewById(R.id.item_common_result_thumbnail_iv);
+            holder.tv_clip = view.findViewById(R.id.item_common_result_clip_tv);
+            holder.tv_rank = view.findViewById(R.id.item_common_result_rank_num);
+            holder.tv_time = view.findViewById(R.id.item_common_result_content_time);
+            holder.tv_title = view.findViewById(R.id.item_common_result_content_title);
+            holder.ll_rating = view.findViewById(R.id.item_common_result_content_rating);
+            holder.rb_rating = view.findViewById(R.id.item_common_result_content_rating_star);
+            holder.tv_rating_num = view.findViewById(R.id.item_common_result_content_rating_num);
+            holder.tv_line = view.findViewById(R.id.item_common_result_line);
+            //ディスプレイ基づいて、画像の長さと幅さを設定
+            setView(holder);
+            view.setTag(holder);
+        } else {
+            holder = (ViewHolder) view.getTag();
+        }
+        setData(holder);
+        //各アイアムデータ取得してから、設定
+        ContentsData listContentInfo = listData.get(position);
+        if (!TextUtils.isEmpty(listContentInfo.getRank())) {//ランク
+            holder.tv_rank.setText(listContentInfo.getRank());
+        }
+        if (!TextUtils.isEmpty(listContentInfo.getTime())) {//時間
+            holder.tv_time.setText(listContentInfo.getTime());
+        }
+        if (!TextUtils.isEmpty(listContentInfo.getTitle())) {//タイトル
+            String title = listContentInfo.getTitle() + mContext.getResources().getString(R.string.common_ranking_enter);
+            holder.tv_title.setText(title);
+        }
+        if (!TextUtils.isEmpty(listContentInfo.getRatStar())) {//評価
+            holder.rb_rating.setNumStars(NUM_STARS_TOTAL);
+            holder.rb_rating.setRating(Float.parseFloat(listContentInfo.getRatStar()));
+            holder.tv_rating_num.setText(listContentInfo.getRatStar());
+        }
+        if (!TextUtils.isEmpty(listContentInfo.getThumURL())) {//サムネイル
+            holder.rl_thumbnail.setVisibility(View.VISIBLE);
+            holder.iv_thumbnail.setTag(listContentInfo.getThumURL());
+            Bitmap thumbnailImage = mThumbnailProvider.getThumbnailImage(holder.iv_thumbnail, listContentInfo.getThumURL());
+            if (thumbnailImage != null) {
+                holder.iv_thumbnail.setImageBitmap(thumbnailImage);
+            }
+        } else {
+            holder.rl_thumbnail.setVisibility(View.GONE);
+        }
+        return view;
+    }
+
+    /**
+     * ビューの設定
+     */
+    private void setView(ViewHolder holder) {
+        DisplayMetrics DisplayMetrics = mContext.getResources().getDisplayMetrics();
+        float density = DisplayMetrics.density;
+        float mWidth = (float) DisplayMetrics.widthPixels / THUMBNAIL_WIDTH;
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams((int) mWidth, (int) mWidth / THUMBNAIL_HEIGHT);
+        layoutParams.setMargins((int) density * THUMBNAIL_MARGINLEFT, (int) density * THUMBNAIL_MARGINTOP, (int) density * THUMBNAIL_MARGINRIGHT, (int) density * THUMBNAIL_MARGINBOTTOM);
+        holder.rl_thumbnail.setLayoutParams(layoutParams);
+        layoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, (int) density * LINE_HEIGHT);
+        layoutParams.setMargins((int) density * THUMBNAIL_MARGINLEFT, THUMBNAIL_MARGIN0, THUMBNAIL_MARGIN0, THUMBNAIL_MARGIN0);
+        holder.tv_line.setLayoutParams(layoutParams);
+    }
+
+    /**
+     * データの設定
+     */
+    private void setData(ViewHolder holder) {
+        switch (type) {
+            case TYPE_DAILY_RANK:
+            case TYPE_WEEKLY_RANK:
+                holder.ll_rating.setVisibility(View.GONE);
+                break;
+            case TYPE_VIDEO_RANK:
+                holder.tv_time.setVisibility(View.GONE);
+                break;
+            case TYPE_RENTAL_RANK:
+                holder.tv_rank.setVisibility(View.GONE);
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * ビュー管理クラス
+     */
+    private static class ViewHolder {
+        //サムネイル親レイアウト
+        RelativeLayout rl_thumbnail;
+        //サムネイル
+        ImageView iv_thumbnail;
+        //評価の親レイアウト
+        LinearLayout ll_rating;
+        //評価（星）
+        RatingBar rb_rating;
+        //評価（count）
+        TextView tv_rating_num;
+        //ラベル
+        TextView tv_rank;
+        //サブタイトル
+        TextView tv_time;
+        //メインタイトル
+        TextView tv_title;
+        //操作アイコンボタン
+        TextView tv_clip;
+        //ライン
+        TextView tv_line;
+    }
+}
+
