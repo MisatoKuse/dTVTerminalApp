1aa52d3028c9cd05d05d429fc994b58be1a828e2
chinn <chenlixi@fsi.co.jp>
2017-11-09 18:53:59 +0900

    ([DREM-8]ビデオ一覧) 実装(WebApi、Parser)
    ジャンル毎コンテンツ数取得JsonParserを実装しました
    
    Change-Id: I2f68efe6765d9940ba74c550d340ae12f80a3300

---
+++ dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/dataprovider/data/GenreCountGetMetaData.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2018 NTT DOCOMO, INC. All Rights Reserved.
+ */
+
+package com.nttdocomo.android.tvterminalapp.dataprovider.data;
+
+import com.nttdocomo.android.tvterminalapp.common.DTVTLogger;
+import com.nttdocomo.android.tvterminalapp.utils.DBUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+
+
+public class GenreCountGetMetaData implements Serializable {
+    private static final long serialVersionUID = -2522621200651636807L;
+    private final String CLASS_NAME = getClass().getSimpleName();
+    private static final String SET_DATA = ".setData";
+    private String mGenreId;//ジャンルID
+    private int mCount;//コンテンツ数
+
+    public static final String GENRE_COUNT_GET_META_DATA_GENRE_ID = "genre_id";
+    public static final String GENRE_COUNT_GET_META_DATA_COUNT = "count";
+    public static final String[] mRootPara = {GENRE_COUNT_GET_META_DATA_GENRE_ID, GENRE_COUNT_GET_META_DATA_COUNT};
+
+    public String getGenreId() {
+        return mGenreId;
+    }
+
+    public void setGenreId(String mGenreId) {
+        this.mGenreId = mGenreId;
+    }
+
+    public int getCount() {
+        return mCount;
+    }
+
+    public void setCount(int mCount) {
+        this.mCount = mCount;
+    }
+
+    /**
+     * キーとキーの値をメンバーにセットする
+     *
+     * @param key  キー
+     * @param data キーの値
+     */
+    private void setMember(String key, Object data) {
+        if (key.isEmpty()) {
+            return;
+        } else {
+            switch (key) {
+                case GENRE_COUNT_GET_META_DATA_GENRE_ID:
+                    mGenreId = (String) data;
+                    break;
+                case GENRE_COUNT_GET_META_DATA_COUNT:
+                    mCount = DBUtils.getNumeric(data);
+                    break;
+                default:
+            }
+        }
+    }
+
+    /**
+     * キーの値を取得する
+     *
+     * @param key キー
+     */
+    public Object getMember(String key) {
+        if (key.isEmpty()) {
+            return "";
+        } else {
+            switch (key) {
+                case GENRE_COUNT_GET_META_DATA_GENRE_ID:
+                    return mGenreId;
+                case GENRE_COUNT_GET_META_DATA_COUNT:
+                    return mCount;
+                default:
+                    return "";
+            }
+        }
+    }
+
+    /**
+     * サーバから取得したデータをセット
+     *
+     * @param jsonObj Jsonオブジェクト
+     */
+    public void setData(JSONObject jsonObj) {
+        if (jsonObj != null) {
+            //ジャンル毎コンテンツ数
+            for (String item : mRootPara) {
+                if (!jsonObj.isNull(item)) {
+                    try {
+                        setMember(item, jsonObj.get(item));
+                    } catch (JSONException e) {
+                        DTVTLogger.debug(CLASS_NAME + SET_DATA, e);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/webapiclient/jsonparser/GenreCountGetJsonParser.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 2018 NTT DOCOMO, INC. All Rights Reserved.
+ */
+
+package com.nttdocomo.android.tvterminalapp.webapiclient.jsonparser;
+
+
+import android.os.AsyncTask;
+import com.nttdocomo.android.tvterminalapp.common.DTVTLogger;
+import com.nttdocomo.android.tvterminalapp.dataprovider.data.GenreCountGetMetaData;
+import com.nttdocomo.android.tvterminalapp.dataprovider.data.GenreCountGetResponse;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import static com.nttdocomo.android.tvterminalapp.dataprovider.data.GenreCountGetResponse.GENRE_COUNT_GET_RESPONSE_LIST;
+import static com.nttdocomo.android.tvterminalapp.dataprovider.data.GenreCountGetResponse.GENRE_COUNT_GET_RESPONSE_STATUS;
+
+public class GenreCountGetJsonParser extends AsyncTask<Object, Object, Object> {
+    private final String CLASS_NAME = getClass().getSimpleName();
+    private static final String SEND_RESPONSE = ".sendGenreCountGetResponse";
+    private static final String SEND_STATUS = ".sendStatus";
+    private static final String RESPONSE = ".GenreCountGetResponse";
+    private static final String JSON_OBJECT = ".JSONObject";
+//    private GenreCountGetWebClient.GenreCountGetJsonParserCallback
+//            mGenreCountGetJsonParserCallback;
+    // オブジェクトクラスの定義　
+    private GenreCountGetResponse mGenreCountGetResponse;
+
+    /**
+     * コンストラクタ
+     * <p>
+     * //     * @param genreCountGetJsonParserCallback
+     */
+//    public GenreCountGetJsonParser(GenreCountGetWebClient.
+//                                           GenreCountGetJsonParserCallback
+//　　 genreCountGetJsonParserCallback) { mGenreCountGetJsonParserCallback =
+//                　　　　　　　　　　　　　genreCountGetJsonParserCallback;
+//        mGenreCountGetResponse = new GenreCountGetResponse();
+//    }
+//
+//    @Override
+//    protected void onPostExecute(Object s) {
+//        mGenreCountGetJsonParserCallback.
+//                onGenreCountGetJsonParsed(mGenreCountGetResponse);
+//    }
+    @Override
+    protected Object doInBackground(Object... strings) {
+        String result = (String) strings[0];
+        GenreCountGetResponse response = genreCountGetSender(result);
+        return response;
+    }
+
+    /**
+     * ジャンル毎コンテンツ数取得一覧Jsonデータを解析する
+     *
+     * @param jsonStr ジャンル毎コンテンツ数取得一覧Jsonデータ
+     * @return ジャンル毎コンテンツ数取得一覧取得：正常時レスポンスデータ
+     */
+    public GenreCountGetResponse genreCountGetSender(String jsonStr) {
+
+        mGenreCountGetResponse = new GenreCountGetResponse();
+
+        try {
+            if (jsonStr != null) {
+                JSONObject jsonObj = new JSONObject(jsonStr);
+                if (jsonObj != null) {
+                    sendStatus(jsonObj);
+                    sendGenreCountGetResponse(jsonObj);
+                    return mGenreCountGetResponse;
+                }
+            }
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            DTVTLogger.debug(CLASS_NAME + JSON_OBJECT, e);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            DTVTLogger.debug(CLASS_NAME + RESPONSE, e);
+        }
+        return null;
+    }
+
+    /**
+     * statusの値をジャンルコンテンツ数取得：正常時レスポンスデータオブジェクトに格納
+     *
+     * @param jsonObj APIレスポンス Jsonデータ
+     */
+    public void sendStatus(JSONObject jsonObj) {
+        try {
+            // statusの値を取得しセットする
+            if (!jsonObj.isNull(GENRE_COUNT_GET_RESPONSE_STATUS)) {
+                String status = jsonObj.getString(GENRE_COUNT_GET_RESPONSE_STATUS);
+                if (mGenreCountGetResponse != null) {
+                    mGenreCountGetResponse.setStatus(status);
+                }
+            }
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            DTVTLogger.debug(CLASS_NAME + SEND_STATUS, e);
+        }
+    }
+
+    /**
+     * ジャンルコンテンツ数のListをジャンルコンテンツ数取得：正常時レスポンスデータオブジェクトに格納
+     *
+     * @param jsonObj APIレスポンス Jsonデータ
+     */
+    public void sendGenreCountGetResponse(JSONObject jsonObj) {
+        try {
+            ArrayList<GenreCountGetMetaData> genreCountGetMetaDataList =
+                    new ArrayList<GenreCountGetMetaData>();
+            if (!jsonObj.isNull(GENRE_COUNT_GET_RESPONSE_LIST)) {
+                // ジャンル毎コンテンツ数取得一覧をJSONArrayにパースする
+                JSONArray lists = jsonObj.getJSONArray(
+                        GENRE_COUNT_GET_RESPONSE_LIST);
+                if (lists.length() == 0) {
+                    return;
+                }
+                //ジャンル毎コンテンツ数取得一覧のデータオブジェクトArrayListを生成する
+                for (int i = 0; i < lists.length(); i++) {
+                    GenreCountGetMetaData genreCountGetMetaData =
+                            new GenreCountGetMetaData();
+                    genreCountGetMetaData.setData(lists.getJSONObject(i));
+                    genreCountGetMetaDataList.add(genreCountGetMetaData);
+                }
+                if (mGenreCountGetResponse != null) {
+                    // ジャンル毎コンテンツ数取得リストをセットする
+                    mGenreCountGetResponse.setGenreCountGetMetaData(
+                            genreCountGetMetaDataList);
+                }
+            }
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            DTVTLogger.debug(CLASS_NAME + SEND_RESPONSE, e);
+        }
+    }
+
+}
