17a62c237e1827ce547ddd10ecd5f6e1378a0ee6
fsi-erinahin <erinahin@fsi.co.jp>
2017-12-11 22:21:35 +0900

    DREM-847 [結合]([DREM-56]ぷららIF最新版の反映)結合試験実施   <対応内容>  修正コミット：Stringの不要文字を消去 Stringの追加、ビデオクリップからratingが参照できていなかったので修正   <担当者>   日野
    Change-Id: I64064b3c23e5f3ad729b6f5df0f17ac7a513ed57

--- dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/activity/home/RecordReservationListActivity.java
+++ dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/activity/home/RecordReservationListActivity.java
@@ -17,7 +17,6 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.nttdocomo.android.tvterminalapp.activity.player.TvPlayerActivity;
 import com.nttdocomo.android.tvterminalapp.activity.BaseActivity;
 import com.nttdocomo.android.tvterminalapp.R;
 import com.nttdocomo.android.tvterminalapp.adapter.ContentsAdapter;
@@ -138,11 +137,11 @@ public class RecordReservationListActivity extends BaseActivity
     /**
      * 再読み込み時のダイアログ表示処理
      *
-     * @param b
+     * @param bool
      */
-    private void displayMoreData(boolean b) {
+    private void displayMoreData(boolean bool) {
         if (null != mListView && null != mLoadMoreView) {
-            if (b) {
+            if (bool) {
                 mListView.addFooterView(mLoadMoreView);
             } else {
                 mListView.removeFooterView(mLoadMoreView);
@@ -161,11 +160,11 @@ public class RecordReservationListActivity extends BaseActivity
     /**
      * 再読み込み実施フラグ設定
      *
-     * @param b
+     * @param bool
      */
-    private void setCommunicatingStatus(boolean b) {
+    private void setCommunicatingStatus(boolean bool) {
         synchronized (this) {
-            mIsCommunicating = b;
+            mIsCommunicating = bool;
         }
     }
 
