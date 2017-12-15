b720ec4aecca785388d0ba76442c625a28d474fd
fsi-masudaju <masudaju@fsi.co.jp>
2017-12-15 19:42:58 +0900

    DREM-959 [実装]([DREM-14]dアカウント設定アプリ連携)実装・実装レビュー
    dアカウント設定アプリとの連携を行う処理を追加しました。
    
    ・OTTの取得処理
    ・dアカウント状態変化通知用レシーバーの登録処理
    
    Change-Id: I1590c3d8b707d7c0b366c1966ba4936a0e817c26

---
+++ dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/webapiclient/daccount/DaccountReceiver.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2018 NTT DOCOMO, INC. All Rights Reserved.
+ */
+
+package com.nttdocomo.android.tvterminalapp.webapiclient.daccount;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * dアカウントで状況の変化が発生した場合の通知を受け取るレシーバー
+ * TODO: dアカウント用にマニフェストファイルの設定を行ったのに応じて生成された。中身はこれから
+ */
+public class DaccountReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
