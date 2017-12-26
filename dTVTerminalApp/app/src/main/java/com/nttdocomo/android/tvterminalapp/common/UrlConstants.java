247ac72cb16117a27ca60bac6f5780430a404cb1
fsi-samasaka <samasaka@fsi.co.jp>
2017-12-26 14:25:47 +0900

    DREM-998 検索レコメンドサーバのURL差し替え
    クローズされるURLに代わり、新しいURLへと差し替え
    
    Change-Id: I3845f1c1e4c6b5a99515a7e92ac975e9a2ea9ef3

--- dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/common/UrlConstants.java
+++ dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/common/UrlConstants.java
@@ -17,7 +17,8 @@ public class UrlConstants {
          */
         public static final String RECOMMEND_LIST_GET_URL =
                 //"http://ve.m.support2.smt.docomo.ne.jp/srermd/recommend/index.do";
-                "https://ve.m.support2.smt.docomo.ne.jp/srermd/recommend/index.do";
+                //"https://ve.m.support2.smt.docomo.ne.jp/srermd/recommend/index.do";
+                "https://ve.m.service.smt.docomo.ne.jp/srermd/recommend/index.do";
 
         //TODO: 本物のぷららサーバーが提供されるまでは、テストサーバーのアドレスを指定する
         /**
