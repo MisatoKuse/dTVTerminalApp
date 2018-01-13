aa897b3ca02a8ac033c6b124972fa646db4c2ff5
fsi-erinahin <erinahin@fsi.co.jp>
2018-01-13 11:48:37 +0900

    gerritのレビューなしであげてしまったので一旦巻き戻し
    Change-Id: I941282fa015d44b0a5c2a3b44f7a5b7179bc9811

--- dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/activity/launch/DAccountReSettingActivity.java
+++ dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/activity/launch/DAccountReSettingActivity.java
@@ -4,41 +4,28 @@
 
 package com.nttdocomo.android.tvterminalapp.activity.launch;
 
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.nttdocomo.android.tvterminalapp.R;
 import com.nttdocomo.android.tvterminalapp.activity.temp.DAccountAppliActivity;
 import com.nttdocomo.android.tvterminalapp.activity.BaseActivity;
-import com.nttdocomo.android.tvterminalapp.common.CustomDialog;
 
 public class DAccountReSettingActivity extends BaseActivity implements View.OnClickListener {
 
-    // STBにdアカウントを登録するには
-    private TextView mDAccountRegistrationhelp = null;
-    // アクティベーションコードを入力する
-    private TextView mDAccountInputActivationCode = null;
-    // 再度ペアリングを実行する
-    private TextView mDAccountAgainPairing = null;
-    // 別のdアカウントを実行する
-    private TextView mDAccountLoginOtherAccount = null;
-
-    //Dアカウントアプリ Package名
-    private static final String D_ACCOUNT_APP_PACKAGE_NAME = "com.nttdocomo.android.idmanager";
-    //Dアカウントアプリ Activity名
-    private static final String D_ACCOUNT_APP_ACTIVITY_NAME=".activity.DocomoIdTopActivity";
-    //DアカウントアプリURI
-    private static final String D_ACCOUNT_APP_URI = "market://details?id=com.nttdocomo.android.idmanager";
+    private Button mOtherDAccountDAccounReSettingActivity=null;
+    private Button mDAccountRegDAccountReSettingActivity=null;
+    private Button mReturnDAccountReSettingActivity=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.daccount_resetting_main_layout);
+
         setContents();
+
     }
 
     @Override
@@ -52,41 +39,40 @@ public class DAccountReSettingActivity extends BaseActivity implements View.OnCl
     }
 
     private void setContents() {
-        mDAccountRegistrationhelp = (TextView) findViewById(R.id.d_account_registration_help);
-        mDAccountRegistrationhelp.setOnClickListener(this);
+        TextView title= (TextView)findViewById(R.id.titleDAccountReSettingActivity);
+        title.setText(getScreenTitle());
 
-        mDAccountInputActivationCode = (TextView) findViewById(R.id.d_account_input_activation_code);
-        mDAccountInputActivationCode.setOnClickListener(this);
+        mOtherDAccountDAccounReSettingActivity=(Button)findViewById(R.id.otherDAccountDAccounReSettingActivity);
+        mOtherDAccountDAccounReSettingActivity.setOnClickListener(this);
 
-        mDAccountAgainPairing = (TextView) findViewById(R.id.d_account_again_pairing);
-        mDAccountAgainPairing.setOnClickListener(this);
+        mDAccountRegDAccountReSettingActivity=(Button)findViewById(R.id.dAccountRegDAccountReSettingActivity);
+        mDAccountRegDAccountReSettingActivity.setOnClickListener(this);
 
-        mDAccountLoginOtherAccount = (TextView) findViewById(R.id.d_account_login_other_account);
-        mDAccountLoginOtherAccount.setOnClickListener(this);
+        mReturnDAccountReSettingActivity=(Button)findViewById(R.id.returnDAccountReSettingActivity);
+        mReturnDAccountReSettingActivity.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
-        if (v.equals(mDAccountRegistrationhelp)) {
-            // ヘルプ画面に遷移
+        if(v.equals(mOtherDAccountDAccounReSettingActivity)){
+            onOtherDAccountButton();
+        } else if(v.equals(mDAccountRegDAccountReSettingActivity)){
             onDAccountRegButton();
-        } else if (v.equals(mDAccountInputActivationCode)) {
-            // アカウントアプリを起動する
-            onDAccountRegton();
-        } else if (v.equals(mDAccountAgainPairing)) {
-            // 再度ペアリング
+        } else if(v.equals(mReturnDAccountReSettingActivity)){
             onReturnButton();
-        } else if (v.equals(mDAccountLoginOtherAccount)) {
-            // アカウントアプリを起動する
-            onDAccountRegton();
         }
     }
 
-    /**
-     * ペアリングの再実行を行う
-     */
     private void onReturnButton() {
         startActivity(STBSelectActivity.class, null);
+    }
+
+    /**
+     * 別のdアカウントでログイン
+     */
+    private void onOtherDAccountButton() {
+        startActivity(DAccountAppliActivity.class, null);
     }
 
     /**
@@ -96,28 +82,4 @@ public class DAccountReSettingActivity extends BaseActivity implements View.OnCl
         startActivity(DAccountSettingHelpActivity.class, null);
     }
 
-    /**
-     * dアカウントアプリを起動する
-     */
-    private void onDAccountRegton() {
-        Intent intent = new Intent();
-        intent.setClassName(D_ACCOUNT_APP_PACKAGE_NAME,
-                D_ACCOUNT_APP_PACKAGE_NAME + D_ACCOUNT_APP_ACTIVITY_NAME);
-        try {
-            startActivity(intent);
-        } catch (ActivityNotFoundException e){
-            //　アプリが無ければインストール画面に誘導
-            CustomDialog dAccountUninstallDialog = new CustomDialog(this, CustomDialog.DialogType.CONFIRM);
-            dAccountUninstallDialog.setContent(getResources().getString(R.string.main_setting_d_account_message));
-            dAccountUninstallDialog.setOkCallBack(new CustomDialog.ApiOKCallback() {
-                @Override
-                public void onOKCallback(boolean isOK) {
-                    Uri uri = Uri.parse(D_ACCOUNT_APP_URI);
-                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                    startActivity(intent);
-                }
-            });
-            dAccountUninstallDialog.showDialog();
-        }
-    }
 }
