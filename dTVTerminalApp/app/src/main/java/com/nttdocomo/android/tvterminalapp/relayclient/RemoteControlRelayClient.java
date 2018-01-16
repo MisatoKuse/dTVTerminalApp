ec89e42dc5e5d835782678df08969184afda44c3
chinn <chenlixi@fsi.co.jp>
2018-01-16 14:45:56 +0900

    [DREM-13]STB接続状態表示
    dアカチェック処理をプッシュ
    
    Change-Id: Ie7f13a848958071c1f4707374afe3e8449dc7bb4

--- dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/relayclient/RemoteControlRelayClient.java
+++ dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/relayclient/RemoteControlRelayClient.java
@@ -509,6 +509,7 @@ public class RemoteControlRelayClient {
      * @return
      */
     public void isUserAccountExistRequest(Context context) {
+
         String requestParam;
 
         //ユーザID取得
@@ -666,7 +667,9 @@ public class RemoteControlRelayClient {
      * アプリ起動要求送信スレッドを開始.
      */
     private void sendStartApplicationRequest(String requestParam) {
-        new Thread(new StartApplicationRequestTask(requestParam)).start();
+        Thread mThread = new Thread(new StartApplicationRequestTask(requestParam));
+        mThread.start();
+//        new Thread(new StartApplicationRequestTask(requestParam)).start();
     }
 
     /**
