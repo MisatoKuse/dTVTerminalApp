c67de26534248e8d8211f968d03393608f31fecf
ouweiyan <ouweiyan@fsi.co.jp>
2018-01-19 17:40:32 +0900

    DREM-1098 ■録画一覧(リモートブラウジング)(c++クラスpush)
    Change-Id: I2634c9203cdca5aac20a99de0abf0d9786f46cf7

---
+++ dTVTerminalApp/app/src/main/cpp/src/remote/CommonRm.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2018 NTT DOCOMO, INC. All Rights Reserved.
+ */
+
+#ifndef DTVTERMINALAPP_COMMONRM_H
+#define DTVTERMINALAPP_COMMONRM_H
+
+
+namespace dtvt {
+    //================================== define begin ==================================//
+
+    //=================================== define end ===================================//
+
+    //================================== type define begin ==================================//
+
+    //=================================== type define end ===================================//
+}
+
+#endif //DTVTERMINALAPP_COMMONRM_H
