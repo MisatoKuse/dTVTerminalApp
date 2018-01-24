8ae97b872f84f8315efc953c1a00640e086e19d0
fsi-samasaka <samasaka@fsi.co.jp>
2018-01-24 21:01:56 +0900

    DREM-1284 [その他](その他作業)ダイアログの表示の標準UI化
    カスタムダイアログのUIを標準UIに修正しました。
    不必要になった独自レイアウトを削除しました。
    
    Change-Id: I41cb3b34bd7a011069111d654f39c11c6c619ced

--- dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/common/CustomDialog.java
+++ dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/common/CustomDialog.java
@@ -8,256 +8,340 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnKeyListener;
-import android.graphics.Color;
+import android.content.res.Resources;
 import android.text.TextUtils;
-import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
 import android.view.Window;
-import android.widget.LinearLayout;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.nttdocomo.android.tvterminalapp.R;
 
 import java.util.List;
 
-public class CustomDialog {
-    private Context context;
+/**
+ * カスタムダイアログ.
+ */
+public class CustomDialog implements DialogInterface.OnClickListener, AdapterView.OnItemClickListener {
+
+    /**
+     * コンテキスト.
+     */
+    private Context mContext;
+    /**
+     * ダイアログ.
+     */
     private AlertDialog dialog;
+    /**
+     * ダイアログ種別.
+     */
     private DialogType dialogType;
+    /**
+     * タイトル.
+     */
     private String title;
+    /**
+     * 本文.
+     */
     private String content;
-    private List<String> list;
+    /**
+     * リスト表示時のリスト.
+     */
+    private List<String> list = null;
+    /**
+     * OK押下時のコールバック.
+     */
     private ApiOKCallback apiOKCallback;
+    /**
+     * Cancel押下時のコールバック.
+     */
     private ApiCancelCallback mApiCancelCallback = null;
+    /**
+     * リスト押下時のコールバック.
+     */
     private ApiSelectCallback apiSelectCallback;
+    /**
+     * リストアイテム押下時のコールバック.
+     */
     private ApiItemSelectCallback apiItemSelectCallback;
+    /**
+     * OKボタンに表示する文字列.
+     */
     private String confirmText = null;
+    /**
+     * Cancelボタンに表示する文字列.
+     */
     private String cancelText = null;
+    /**
+     * ダイアログのcancelable設定値.
+     */
     private boolean cancelable = true;
     /**
-     * ダイアログのOKボタン
+     * ダイアログのOKボタン.
      */
     private TextView mOkButton = null;
     /**
-     * ダイアログのキャンセルボタン
+     * ダイアログのキャンセルボタン.
      */
     private TextView mCancelButton = null;
-
-    private int cancelVisibility = View.VISIBLE;
+    /**
+     * OKボタンの表示/非表示判定.
+     */
     private int confirmVisibility = View.VISIBLE;
-    private boolean isCancelVisibilityChanged = false;
-    private boolean isConfirmVisibilityChanged = false;
+    /**
+     * Cancelボタンの表示/非表示判定.
+     */
+    private int cancelVisibility = View.VISIBLE;
 
     /**
-     * OKを返却するためのコールバック
+     * OKボタン押下を返却するためのコールバック.
      */
     public interface ApiOKCallback {
+        /**
+         * OKボタン押下のコールバック.
+         * @param isOK true
+         */
         void onOKCallback(boolean isOK);
     }
 
     /**
-     * Cancelを返却するためのコールバック
+     * Cancelボタン押下を返却するためのコールバック.
      */
     public interface ApiCancelCallback {
+        /**
+         * Cancelボタン押下時のコールバック.
+         */
         void onCancelCallback();
     }
 
     /**
-     * SELECTを返却するためのコールバック
+     * SELECTを返却するためのコールバック.
      */
     public interface ApiSelectCallback {
+        /**
+         * リスト選択時のコールバック.
+         * @param position 押下されたリストアイテム
+         */
         void onSelectCallback(int position);
     }
 
     /**
-     * SELECTを返却するためのコールバック
+     * SELECTを返却するためのコールバック.
      */
     public interface ApiItemSelectCallback {
+        /**
+         * リストアイテムが選択時のコールバック.
+         * @param dialog ダイアログ
+         * @param position 押下されたリストアイテム
+         */
         void onItemSelectCallback(AlertDialog dialog, int position);
     }
 
+    /**
+     * OKボタン押下のコールバック.
+     *
+     * @param apiOKCallback OKボタン押下のコールバック
+     */
     public void setOkCallBack(ApiOKCallback apiOKCallback) {
         this.apiOKCallback = apiOKCallback;
     }
 
+    /**
+     * Cancelボタン押下を返却するためのコールバック.
+     *
+     * @param apiCancelCallback Cancelボタン押下を返却するためのコールバック
+     */
     public void setApiCancelCallback(ApiCancelCallback apiCancelCallback) {
         this.mApiCancelCallback = apiCancelCallback;
     }
 
+    /**
+     * リスト選択時のコールバック..
+     *
+     * @param apiSelectCallback リスト選択時のコールバック
+     */
     public void setSelectCallBack(ApiSelectCallback apiSelectCallback) {
         this.apiSelectCallback = apiSelectCallback;
     }
 
+    /**
+     * リストアイテムが選択時のコールバック.
+     *
+     * @param callback リストアイテムが選択時のコールバック
+     */
     public void setItemSelectCallback(ApiItemSelectCallback callback) {
         this.apiItemSelectCallback = callback;
     }
 
+    /**
+     * コンストラクタ.
+     *
+     * @param context コンテキスト
+     * @param dialogType ダイアログタイプ
+     */
     public CustomDialog(Context context, DialogType dialogType) {
-        this.context = context;
+        this.mContext = context;
         this.dialogType = dialogType;
     }
 
+    /**
+     * ダイアログのタイトルを設定.
+     *
+     * @param title タイトル
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * ダイアログに表示する本文を設定.
+     *
+     * @param content 本文
+     */
     public void setContent(String content) {
         this.content = content;
     }
 
+    /**
+     * ダイアログに表示するリストを設定.
+     *
+     * @param list リスト
+     */
     public void setSelectData(List<String> list) {
         this.list = list;
     }
 
+    /**
+     * ダイアログタイプ.
+     */
     public enum DialogType {
+        /**
+         * ダイアログタイプ：エラー(単一ボタン).
+         */
         ERROR,          //エラーダイアログ
+        /**
+         * ダイアログタイプ：確認(OK/Cancelボタン).
+         */
         CONFIRM,        //確認ダイアログ
+        /**
+         * ダイアログタイプ：リスト(リスト表示、OK/Cancelは任意).
+         */
         SELECT          //選択ダイアログ
     }
 
     /**
-     * ダイアログの表示
+     * ダイアログの表示.
      */
     public void showDialog() {
-        dialog = new AlertDialog.Builder(context).create();
-        dialog.setCanceledOnTouchOutside(true);
-        dialog.setOnKeyListener(keyListener);
-        dialog.show();
-        Window window = dialog.getWindow();
-        if (window != null) {
-            window.setContentView(R.layout.custom_dialog);
-            initView(window);
-        } else {
-            dismissDialog();
-        }
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(mContext);
+        initView(dialogBuilder);
     }
 
     /**
-     * ダイアログビューの初期化
-     * @param window スクリーン
+     * ダイアログビューの初期化.
+     * @param dialogBuilder スクリーン
      */
-    private void initView(Window window) {
-        TextView titleTextView = window.findViewById(R.id.custom_dialog_title_tv);
-        TextView contentTextView = window.findViewById(R.id.custom_dialog_content);
-        if (TextUtils.isEmpty(title)) {
-            titleTextView.setVisibility(View.GONE);
-        } else {
-            titleTextView.setText(title);
+    private void initView(AlertDialog.Builder dialogBuilder) {
+        if (TextUtils.isEmpty(confirmText)) {
+            confirmText = mContext.getString(R.string.custom_dialog_ok);
         }
-        if (TextUtils.isEmpty(content)) {
-            contentTextView.setVisibility(View.GONE);
-        } else {
-            contentTextView.setText(content);
+        if (TextUtils.isEmpty(cancelText)) {
+            cancelText = mContext.getString(R.string.custom_dialog_cancel);
         }
-        mOkButton = window.findViewById(R.id.custom_dialog_confirm);
-        mCancelButton = window.findViewById(R.id.custom_dialog_cancel);
         switch (dialogType) {
             case ERROR:
-                mCancelButton.setVisibility(View.GONE);
-                if (confirmText != null) {
-                    mOkButton.setText(confirmText);
-                }
+                dialogBuilder.setPositiveButton(confirmText, this);
                 break;
             case SELECT:
-                if (isConfirmVisibilityChanged) {
-                    mOkButton.setVisibility(confirmVisibility);
-                } else {
-                    mOkButton.setVisibility(View.GONE);
+                if (confirmVisibility == View.VISIBLE) {
+                    dialogBuilder.setPositiveButton(confirmText, this);
                 }
-                if (isCancelVisibilityChanged) {
-                    mCancelButton.setVisibility(cancelVisibility);
+                if (cancelVisibility == View.VISIBLE) {
+                    dialogBuilder.setNegativeButton(cancelText, this);
                 }
-                window.findViewById(R.id.custom_dialog_line_separete).setVisibility(View.VISIBLE);
-                window.findViewById(R.id.custom_dialog_sl).setVisibility(View.VISIBLE);
                 if (list != null) {
-                    LinearLayout linearLayout = window.findViewById(R.id.custom_dialog_ll);
-                    for (int i = 0; i < list.size(); i++) {
-                        TextView tabTextView = new TextView(context);
-                        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                                LinearLayout.LayoutParams.MATCH_PARENT,
-                                LinearLayout.LayoutParams.WRAP_CONTENT);
-                        tabTextView.setLayoutParams(params);
-                        tabTextView.setPadding(0, (int) context.getResources().getDimension(R.dimen.contents_detail_5dp),
-                                0, (int) context.getResources().getDimension(R.dimen.contents_detail_5dp));
-                        tabTextView.setText(list.get(i));
-                        tabTextView.setBackgroundResource(R.drawable.rectangele_white);
-                        tabTextView.setGravity(Gravity.CENTER);
-                        tabTextView.setTextColor(Color.BLUE);
-                        tabTextView.setTag(i);
-                        tabTextView.setOnClickListener(new OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                int position = (int) view.getTag();
-                                if (apiSelectCallback != null) {
-                                    apiSelectCallback.onSelectCallback(position);
-                                }
-                                if (apiItemSelectCallback != null) {
-                                    apiItemSelectCallback.onItemSelectCallback(dialog, position);
-                                }
-                            }
-                        });
-                        linearLayout.addView(tabTextView);
-                    }
+                    ListView listView = new ListView(mContext);
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(mContext,
+                            android.R.layout.simple_list_item_1, list);
+                    listView.setAdapter(arrayAdapter);
+                    listView.setOnItemClickListener(this);
+                    dialogBuilder.setView(listView);
                 }
                 break;
             case CONFIRM:
-                if (confirmText != null) {
-                    mOkButton.setText(confirmText);
-                }
-                if (cancelText != null) {
-                    mCancelButton.setText(cancelText);
-                }
+                dialogBuilder.setPositiveButton(confirmText, this);
+                dialogBuilder.setNegativeButton(cancelText, this);
                 break;
             default:
                 break;
-
         }
-        if (mOkButton.getVisibility() == View.VISIBLE) {
-            mOkButton.setOnClickListener(new OnClickListener() {
-
-                @Override
-                public void onClick(View view) {
-                    dismissDialog();
-                    if (apiOKCallback != null) {
-                        apiOKCallback.onOKCallback(true);
-                    }
-                }
-            });
-        }
-        if (mCancelButton.getVisibility() == View.VISIBLE) {
-            mCancelButton.setOnClickListener(new OnClickListener() {
-
-                @Override
-                public void onClick(View view) {
-                    dismissDialog();
-                    if (mApiCancelCallback != null) {
-                        mApiCancelCallback.onCancelCallback();
-                    }
-                }
-            });
-        }
+        dialog = dialogBuilder.create();
         dialog.setCancelable(cancelable);
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.setOnKeyListener(keyListener);
 
-        /*WindowManager.LayoutParams lp = window.getAttributes();
-        lp.width = 800;
-        window.setAttributes(lp);*/
-        window.setLayout(ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT);
-        window.setGravity(Gravity.CENTER);
+        if (TextUtils.isEmpty(title)) {
+            Window window = dialog.getWindow();
+            if (window != null) {
+                window.requestFeature(Window.FEATURE_NO_TITLE);
+            }
+        } else {
+            dialog.setTitle(title);
+        }
+        if (TextUtils.isEmpty(content)) {
+            dialog.setMessage("");
+        } else {
+            dialog.setMessage(content);
+        }
+
+        dialog.show();
     }
 
     /**
-     * ダイアログを閉じる
+     * ダイアログを閉じる.
      */
-    public void dismissDialog() {
+    private void dismissDialog() {
         if (dialog != null) {
             dialog.dismiss();
         }
     }
 
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        switch (which) {
+            case AlertDialog.BUTTON_POSITIVE:
+                dismissDialog();
+                if (apiOKCallback != null) {
+                    apiOKCallback.onOKCallback(true);
+                }
+                break;
+            case AlertDialog.BUTTON_NEGATIVE:
+                dismissDialog();
+                if (mApiCancelCallback != null) {
+                    mApiCancelCallback.onCancelCallback();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        if (apiSelectCallback != null) {
+            apiSelectCallback.onSelectCallback(position);
+        }
+        if (apiItemSelectCallback != null) {
+            apiItemSelectCallback.onItemSelectCallback(dialog, position);
+        }
+    }
+
+    /**
+     * バックキー押下時の動作.
+     */
     private static OnKeyListener keyListener = new OnKeyListener() {
         @Override
         public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
@@ -272,41 +356,48 @@ public class CustomDialog {
     };
 
     /**
-     * DialogType.CONFIRMのconfirmテキストを変更
+     * DialogType.CONFIRMのconfirmテキストを変更.
+     *
      * @param resId リソースID
      */
     public void setConfirmText(int resId) {
-        confirmText = context.getResources().getString(resId);
+        Resources resources = mContext.getResources();
+        confirmText = resources.getString(resId);
     }
 
     /**
-     * DialogType.CONFIRMのcancelテキストを変更
+     * DialogType.CONFIRMのcancelテキストを変更.
      * @param resId リソースID
      */
     public void setCancelText(int resId) {
-        cancelText = context.getResources().getString(resId);
+        Resources resources = mContext.getResources();
+        cancelText = resources.getString(resId);
     }
 
     /**
-     * Dialogのcancelableを変更
+     * Dialogのcancelableを変更.
+     *
+     * @param cancelable キャンセル可否設定
      */
     public void setCancelable(boolean cancelable) {
         this.cancelable = cancelable;
     }
 
     /**
-     * confirmの表示/非表示を設定
+     * confirmの表示/非表示を設定.
+     *
+     * @param visibility 表示/非表示設定
      */
     public void setConfirmVisibility(int visibility) {
-        isConfirmVisibilityChanged = true;
         confirmVisibility = visibility;
     }
 
     /**
-     * cancelの表示/非表示を設定
+     * cancelの表示/非表示を設定.
+     *
+     * @param visibility 表示/非表示設定
      */
     public void setCancelVisibility(int visibility) {
-        isCancelVisibilityChanged = true;
         cancelVisibility = visibility;
     }
 
@@ -316,8 +407,8 @@ public class CustomDialog {
      * @param text 表示文言
      */
     public void setOkButtonText(final String text) {
-        if (mOkButton != null) {
-            mOkButton.setText(text);
+        if (confirmText != null) {
+            confirmText = text;
         }
     }
 
@@ -327,8 +418,8 @@ public class CustomDialog {
      * @param text 表示文言
      */
     public void setCancelButtonText(final String text) {
-        if (mCancelButton != null) {
-            mCancelButton.setText(text);
+        if (cancelText != null) {
+            cancelText = text;
         }
     }
 }
