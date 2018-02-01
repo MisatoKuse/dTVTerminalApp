948fbbf234fab73e4fea256f27031be7e35ace99
fsi-samasaka <samasaka@fsi.co.jp>
2018-02-01 11:59:07 +0900

    DREM-1612 [実装]([DREM-32-6] WebView表示画面(ヘルプ等))WebView表示画面の実装
    「ドコモテレビターミナルにdアカウントを登録するには」のヘルプ画面を実装
    webviewの下部に表示されていたwebviewコントローラを既存の画面から削除
    
    Change-Id: I3b8b386f865d47734c326bd062b7285f8da30268

--- dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/activity/launch/DAccountReSettingActivity.java
+++ dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/activity/launch/DAccountReSettingActivity.java
@@ -6,6 +6,7 @@ package com.nttdocomo.android.tvterminalapp.activity.launch;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -15,28 +16,53 @@ import com.nttdocomo.android.tvterminalapp.R;
 import com.nttdocomo.android.tvterminalapp.activity.BaseActivity;
 import com.nttdocomo.android.tvterminalapp.view.CustomDialog;
 
+/**
+ * 端末に登録されているdアカウントとSTBに登録されているdアカウントが一致しなかった場合に表示される.
+ */
 public class DAccountReSettingActivity extends BaseActivity implements View.OnClickListener {
 
-    // STBにdアカウントを登録するには
+    /**
+     * STBにdアカウントを登録するには.
+     */
     private TextView mDAccountRegistrationhelp = null;
-    // アクティベーションコードを入力する
+    /**
+     * アクティベーションコードを入力する.
+     */
     private TextView mDAccountInputActivationCode = null;
-    // 再度ペアリングを実行する
+    /**
+     * 再度ペアリングを実行する.
+     */
     private TextView mDAccountAgainPairing = null;
-    // 別のdアカウントを実行する
+    /**
+     * 別のdアカウントを実行する.
+     */
     private TextView mDAccountLoginOtherAccount = null;
 
-    //Dアカウントアプリ Package名
+    /**
+     * Dアカウントアプリ Package名.
+     */
     private static final String D_ACCOUNT_APP_PACKAGE_NAME = "com.nttdocomo.android.idmanager";
-    //Dアカウントアプリ Activity名
+    /**
+     * Dアカウントアプリ Activity名.
+     */
     private static final String D_ACCOUNT_APP_ACTIVITY_NAME = ".activity.DocomoIdTopActivity";
-    //DアカウントアプリURI
+    /**
+     * DアカウントアプリURI.
+     */
     private static final String D_ACCOUNT_APP_URI = "market://details?id=com.nttdocomo.android.idmanager";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.daccount_resetting_main_layout);
+
+        //Headerの設定
+        //TODO ヘッダーのタイトル名変更対応(BaseActivity側で変更予定のため仮)
+        setTitleText("ドコテレ画像");
+        enableHeaderBackIcon(true);
+        enableStbStatusIcon(false);
+        enableGlobalMenuIcon(false);
+
         setContents();
     }
 
@@ -50,22 +76,25 @@ public class DAccountReSettingActivity extends BaseActivity implements View.OnCl
         return getString(R.string.str_d_account_resetting_title);
     }
 
+    /**
+     * 各クリックリスナーを登録する.
+     */
     private void setContents() {
-        mDAccountRegistrationhelp = (TextView) findViewById(R.id.d_account_registration_help);
+        mDAccountRegistrationhelp = findViewById(R.id.d_account_registration_help);
         mDAccountRegistrationhelp.setOnClickListener(this);
 
-        mDAccountInputActivationCode = (TextView) findViewById(R.id.d_account_input_activation_code);
+        mDAccountInputActivationCode = findViewById(R.id.d_account_input_activation_code);
         mDAccountInputActivationCode.setOnClickListener(this);
 
-        mDAccountAgainPairing = (TextView) findViewById(R.id.d_account_again_pairing);
+        mDAccountAgainPairing = findViewById(R.id.d_account_again_pairing);
         mDAccountAgainPairing.setOnClickListener(this);
 
-        mDAccountLoginOtherAccount = (TextView) findViewById(R.id.d_account_login_other_account);
+        mDAccountLoginOtherAccount = findViewById(R.id.d_account_login_other_account);
         mDAccountLoginOtherAccount.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View v) {
+    public void onClick(final View v) {
         if (v.equals(mDAccountRegistrationhelp)) {
             // ヘルプ画面に遷移
             onDAccountRegButton();
@@ -82,7 +111,7 @@ public class DAccountReSettingActivity extends BaseActivity implements View.OnCl
     }
 
     /**
-     * ペアリングの再実行を行う
+     * ペアリングの再実行を行う.
      */
     private void onReturnButton() {
         startActivity(STBSelectActivity.class, null);
@@ -90,14 +119,14 @@ public class DAccountReSettingActivity extends BaseActivity implements View.OnCl
     }
 
     /**
-     * dアカウント設定ヘルプ
+     * dアカウント設定ヘルプ.
      */
     private void onDAccountRegButton() {
         startActivity(DAccountSettingHelpActivity.class, null);
     }
 
     /**
-     * dアカウントアプリを起動する
+     * dアカウントアプリを起動する.
      */
     private void onDAccountRegton() {
         Intent intent = new Intent();
@@ -108,10 +137,11 @@ public class DAccountReSettingActivity extends BaseActivity implements View.OnCl
         } catch (ActivityNotFoundException e) {
             //　アプリが無ければインストール画面に誘導
             CustomDialog dAccountUninstallDialog = new CustomDialog(this, CustomDialog.DialogType.CONFIRM);
-            dAccountUninstallDialog.setContent(getResources().getString(R.string.main_setting_d_account_message));
+            Resources resources = getResources();
+            dAccountUninstallDialog.setContent(resources.getString(R.string.main_setting_d_account_message));
             dAccountUninstallDialog.setOkCallBack(new CustomDialog.ApiOKCallback() {
                 @Override
-                public void onOKCallback(boolean isOK) {
+                public void onOKCallback(final boolean isOK) {
                     Uri uri = Uri.parse(D_ACCOUNT_APP_URI);
                     Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                     startActivity(intent);
--- dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/activity/launch/DAccountSettingHelpActivity.java
+++ dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/activity/launch/DAccountSettingHelpActivity.java
@@ -5,22 +5,57 @@
 package com.nttdocomo.android.tvterminalapp.activity.launch;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import com.nttdocomo.android.tvterminalapp.R;
 import com.nttdocomo.android.tvterminalapp.activity.BaseActivity;
 
-
+/**
+ * ドコモテレビターミナルにdアカウントを登録するには 画面.
+ */
 public class DAccountSettingHelpActivity extends BaseActivity {
 
+    /**
+     * WebView.
+     */
+    private WebView dAccountHelpPageWebView = null;
+    /**
+     * 表示するWebPageのURL.
+     * TODO 仮のURL
+     */
+    private final static String STB_REGIST_D_ACCOUNT_URL = "https://www.nttdocomo.co.jp/";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.daccount_help_main_layout);
-        WebView dAccountHelpPageWebView = (WebView)findViewById(R.id.d_account_help_webView);
+
+        dAccountHelpPageWebView = findViewById(R.id.stb_d_account_regist_help_webview);
         dAccountHelpPageWebView.setWebViewClient(new WebViewClient());
-        //TODO ヘルプページ webview URL指定
-        dAccountHelpPageWebView.loadUrl("https://www.nttdocomo.co.jp/");
+        WebSettings webSettings = dAccountHelpPageWebView.getSettings();
+        webSettings.setJavaScriptEnabled(false);
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        dAccountHelpPageWebView.loadUrl(STB_REGIST_D_ACCOUNT_URL);
+
+        //Headerの設定
+        //TODO ヘッダーのタイトル名変更対応(BaseActivity側で変更予定のため仮)
+        setTitleText("ドコテレ画像");
+        enableHeaderBackIcon(true);
+        enableStbStatusIcon(false);
+        enableGlobalMenuIcon(false);
+    }
+
+    @Override
+    public boolean onKeyDown(final int keyCode, final KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (dAccountHelpPageWebView.canGoBack()) {
+                dAccountHelpPageWebView.goBack();
+                return false;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
