37d99606436e1e23f80e8219fde34f8608d747dd
yanase <ymasatos@fsi.co.jp>
2018-04-16 19:21:51 +0900

    DREM-2011 コンテンツ配信期限の表示
    Homeで落ちる不具合修正
    
    Change-Id: I62fc0a5c7c0604f868cfb29d78865424c31ffaf6

--- dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/datamanager/select/RentalListDataManager.java
+++ dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/datamanager/select/RentalListDataManager.java
@@ -53,6 +53,7 @@ public class RentalListDataManager {
                 JsonConstants.META_RESPONSE_SERVICE_ID, JsonConstants.META_RESPONSE_EVENT_ID,
                 JsonConstants.META_RESPONSE_TITLE_ID, JsonConstants.META_RESPONSE_R_VALUE,
                 JsonConstants.META_RESPONSE_AVAIL_START_DATE, JsonConstants.META_RESPONSE_AVAIL_END_DATE,
+                JsonConstants.META_RESPONSE_VOD_START_DATE, JsonConstants.META_RESPONSE_VOD_END_DATE,
                 JsonConstants.META_RESPONSE_DTV, JsonConstants.META_RESPONSE_TV_SERVICE,
                 JsonConstants.META_RESPONSE_CONTENT_TYPE, JsonConstants.META_RESPONSE_DTV_TYPE,
                 JsonConstants.META_RESPONSE_RATING, JsonConstants.META_RESPONSE_EPISODE_ID,
