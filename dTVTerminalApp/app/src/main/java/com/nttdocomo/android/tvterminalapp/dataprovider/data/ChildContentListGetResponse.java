5c077d5a1134bd9da5711fe0de004cf31c893118
fsi-chenlixi <chenlixi@fsi.co.jp>
2018-04-17 14:40:37 +0900

    Check Style対応
    Change-Id: If81ba3c204f65704aaa67d3a5cdab05c830b1f41

--- dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/dataprovider/data/ChildContentListGetResponse.java
+++ dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/dataprovider/data/ChildContentListGetResponse.java
@@ -9,32 +9,62 @@ import com.nttdocomo.android.tvterminalapp.struct.Pager;
 import java.util.ArrayList;
 
 /**
- * 子コンテンツ一覧取得APIパース用
+ * 子コンテンツ一覧取得APIパース用.
  */
 public class ChildContentListGetResponse {
 
+    /**
+     * ステータス.
+     */
     private String mStatus;
+    /**
+     * ページャー.
+     */
     private Pager mPager;
+    /**
+     * フルメタデータリスト.
+     */
     private ArrayList<VodMetaFullData> mVodMetaFullData;
-
+    /**
+     * 構造体.
+     */
     public ChildContentListGetResponse() {
         mStatus = "";
         mPager = new Pager();
         mVodMetaFullData = new ArrayList<>();
     }
 
+    /**
+     * ステータスを取得する.
+     * @return ステータス
+     */
     public String getStatus() {
         return mStatus;
     }
 
+    /**
+     * ステータスを設定する.
+     * @param status ステータス
+     */
     public void setStatus(final String status) {
         mStatus = status;
     }
 
+    /**
+     * ページャーを取得する.
+     * @return ページャー
+     */
     public Pager getPager() {
         return mPager;
     }
 
+    /**
+     * ページャーを設定する.
+     * @param  limit レスポンスの最大件数
+     * @param count レスポンス(list)件数
+     * @param offset 取得位置(0～)
+     * @param total 全体の件数
+     */
     public void setPager(final int limit, final int offset, final int count, final int total) {
         mPager.setLimit(limit);
         mPager.setOffset(offset);
@@ -42,10 +72,18 @@ public class ChildContentListGetResponse {
         mPager.setTotal(total);
     }
 
+    /**
+     * フルメタデータを取得する.
+     * @return フルメタデータリスト
+     */
+
     public ArrayList<VodMetaFullData> getVodMetaFullData() {
         return mVodMetaFullData;
     }
-
+    /**
+     * フルメタデータを設定する.
+     * @param vodMetaFullData フルメタデータリスト
+     */
     public void setVodMetaFullData(final ArrayList<VodMetaFullData> vodMetaFullData) {
         mVodMetaFullData = vodMetaFullData;
     }
