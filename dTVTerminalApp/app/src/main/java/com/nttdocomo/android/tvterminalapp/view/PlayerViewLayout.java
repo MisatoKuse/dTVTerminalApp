e0863df71d82329ee9ccdec0a3d945caf525a24a
FSI_鮑 哲 <houtetsu@fsi.co.jp>
2018-07-25 11:50:38 +0900

    DREM-3086 【初回リリース】【Android】特定機種でプレイヤー（横）画面で画面上部に白い枠線が表示される
    Change-Id: I1ac614e96053d9964f38359bdd1e1bf05c61106a

--- dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/view/PlayerViewLayout.java
+++ dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/view/PlayerViewLayout.java
@@ -879,6 +879,9 @@ public class PlayerViewLayout extends RelativeLayout implements View.OnClickList
         if (mIsLandscape) {
             mActivity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
             playerParams.height = getScreenNavHeight();
             if (mPlayerController != null) {
