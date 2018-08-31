424fce38cf63c642e07019acf8edd442ed4015bb
fsi-chenlixi <chenlixi@fsi.co.jp>
2018-08-31 17:30:37 +0900

    DREM-3240  【STEP1.6】【Android】初回起動時にSTB選択画面からdアカウント設定アプリを起動し、戻るボタンを押すとdアカウント設定アプリに遷移する 指摘修正
    Change-Id: I9759b3498ec0ec2284a517e9c537ce756fa14bf9

--- dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/activity/launch/StbSelectActivity.java
+++ dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/activity/launch/StbSelectActivity.java
@@ -841,14 +841,14 @@ public class StbSelectActivity extends BaseActivity implements View.OnClickListe
             //初期値を格納している
             DaccountControl.CheckLastClassEnum checkLastClassEnum =
                     DaccountControl.CheckLastClassEnum.CHECK_SERVICE;
-            if (daccountControl != null) {
+            if (daccountControl != null && mSelectDevice != SELECT_DEVICE_ITEM_DEFAULT) {
                 errorCode = daccountControl.getResult();
                 checkLastClassEnum =  daccountControl.getmResultClass();
                 if (DaccountControl.CheckLastClassEnum.REGIST_SERVICE.equals(checkLastClassEnum)
                     || DaccountControl.CheckLastClassEnum.CHECK_SERVICE.equals(checkLastClassEnum)
                     || DaccountControl.CheckLastClassEnum.ONE_TIME_PASS_WORD.equals(checkLastClassEnum)
                     || DaccountControl.CheckLastClassEnum.CONTROL.equals(checkLastClassEnum)) {
-                    if (errorCode == DaccountUtils.D_ACCOUNT_APP_NOT_FOUND_ERROR_CODE && mSelectDevice != SELECT_DEVICE_ITEM_DEFAULT) {
+                    if (errorCode == DaccountUtils.D_ACCOUNT_APP_NOT_FOUND_ERROR_CODE) {
                         // dアカアプリ未インストールの場合はdアカアプリ起動(関数内でチェックしてストア誘導する画面に飛ぶ).
                         DTVTLogger.debug("dAccountApp is not installed.");
                         checkDAccountApp();
@@ -860,7 +860,7 @@ public class StbSelectActivity extends BaseActivity implements View.OnClickListe
                         // dアカアプリ起動～ドコテレ復帰の間は再起動しない.
                         // また多くの場合はコールバックが復帰後に来るため、ペアリング中の状態以外では起動しない
                         // （Resume時には必ずSTB選択の状態に戻している事を利用）
-                        if (!mIsDAccountAppStarting && mLoadMoreView.getVisibility() == View.VISIBLE && mSelectDevice != SELECT_DEVICE_ITEM_DEFAULT) {
+                        if (!mIsDAccountAppStarting && mLoadMoreView.getVisibility() == View.VISIBLE) {
                             checkDAccountApp();
                         }
                     }
