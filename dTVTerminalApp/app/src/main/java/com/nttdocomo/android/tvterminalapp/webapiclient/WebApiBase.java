1bc0e1d8e8fc733432a8ee337c746ce285f39e6c
ryushiki <ryuushk@fsi.co.jp>
2018-11-05 13:19:12 +0900

    DREM-2441 <AOS> レコメンドサーバ仕様の適用不具合修正
    Change-Id: I23b093339e6c7dab630979ba9358b49c4b64a51e

--- dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/webapiclient/WebApiBase.java
+++ dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/webapiclient/WebApiBase.java
@@ -166,7 +166,7 @@ public class WebApiBase implements HttpThread.HttpThreadFinish {
         //エラーコードだけ抜き出してエラー情報に入れる
         mHttpThread.setXmlErrorCode(str);
 
-        if (null != mWebApiCallback && null != str && !str.isEmpty()) {
+        if (null != mWebApiCallback) {
             mWebApiCallback.onFinish(str);
         }
     }
