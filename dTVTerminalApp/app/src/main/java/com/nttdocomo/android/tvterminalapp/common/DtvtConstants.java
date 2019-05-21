02db12f5d500f229b92ed102cbe80601e05a4251
HiroshiShimanuki <>
2019-05-21 13:41:21 +0900

    DREM-4153 利用規約文言修正不要のため、APPバージョン値修正 APPバージョン　1 → 0
    Change-Id: Ib8a9ade7930ad6241ec277b86c248ef8773eb7c2

--- dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/common/DtvtConstants.java
+++ dTVTerminalApp/app/src/main/java/com/nttdocomo/android/tvterminalapp/common/DtvtConstants.java
@@ -87,7 +87,7 @@ public class DtvtConstants {
     /** cache dataの最新バージョン */
     public static final int NEW_CACHE_DATA_VERDION = 1;
     /** アプリケーションプライバシーポリシー最新バージョン */
-    public static final int NEW_APPLICATION_PRIVACY_POLICY_VERSION = 1;
+    public static final int NEW_APPLICATION_PRIVACY_POLICY_VERSION = 0;
 
     //==================================== 定数定義 End ===================================
 
